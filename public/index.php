2a58c019136d89574bf5d973e8604d49536c9bf8
Alfonso Rios <mriosm1100@outlook.com>
2015-10-02 23:29:33 -0500

    Inicio del repositorio

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso de PHP Orientado a Objetos</title>
+</head>
+<body>
+	<h2><?= 'Hola mundo desde PHP'?></h2>
+</body>
+</html>
